5d0dac18439b88a9486af499b0bb5e375bd7edae
SunilPrakash <sunil.prakash@talentica.com>
2020-04-06 23:11:18 +0530

    New App component for intro to hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import BookList from "./components/BookList";
-import ThemeContextProvider from "./contexts/ThemeContext";
-import ThemeToggle from "./components/ThemeToggle";
-import AuthContextProvider from "./contexts/AuthContext";
+import SongList from "./components/SongList";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <SongList />
     </div>
   );
-}
+};
 
 export default App;
