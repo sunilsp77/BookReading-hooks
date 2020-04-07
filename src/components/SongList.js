9130ffa12b76ad792ad443346c9b7c9631f20dd2
SunilPrakash <sunil.prakash@talentica.com>
2020-04-07 12:30:54 +0530

    Function components and hooks usage

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import uuid from "uuid/v1";
+import NewSongForm from "./NewSongForm";
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "almost home", id: 1 },
+    { title: "memory gospel", id: 2 },
+    { title: "this wild darkness", id: 3 },
+  ]);
+  const [age, setAge] = useState(20);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuid() }]);
+  };
+
+  useEffect(() => {
+    console.log("useEffect hook ran", songs);
+  }, [songs]);
+  useEffect(() => {
+    console.log("useEffect hook ran", age);
+  }, [age]);
   return (
     <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age : {age}</button>
     </div>
   );
 };
