536e55a43687af7fa63d40cfa7f3065b009ca643
SunilPrakash <sunil.prakash@talentica.com>
2020-04-07 12:31:31 +0530

    Consuming context using useContext hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import SongList from "./components/SongList";
+import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <SongList />
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
     </div>
   );
-};
+}
 
 export default App;
