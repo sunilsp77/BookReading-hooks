3bcd0a81df0b319fc04ca0037839a93b0f2d79ae
SunilPrakash <sunil.prakash@talentica.com>
2020-04-07 16:32:14 +0530

    using hooks in the bookList project

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,27 +1,19 @@
 import React, { useContext } from "react";
-
-import { ThemeContext } from "../contexts/ThemeContext";
+import BookDetails from "./BookDetails";
 import { BookContext } from "../contexts/BookContext";
 
-export const BookList = () => {
+const BookList = () => {
   const { books } = useContext(BookContext);
-  const { isLightTheme, light, dark } = useContext(ThemeContext);
-  const theme = isLightTheme ? light : dark;
-  return (
-    <div
-      className="book-list"
-      style={{ background: theme.bg, color: theme.syntax }}
-    >
+  return books.length ? (
+    <div className="book-list">
       <ul>
         {books.map((book) => {
-          return (
-            <li key={book.id} style={{ background: theme.ui }}>
-              {book.title}
-            </li>
-          );
+          return <BookDetails book={book} key={book.id} />;
         })}
       </ul>
     </div>
+  ) : (
+    <div className="empty">No books to read. Hello free time :).</div>
   );
 };
 
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -4,13 +4,18 @@ export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
   const [books, setBooks] = useState([
-    { title: "almost home", id: 1 },
-    { title: "memory gospel", id: 2 },
-    { title: "this wild darkness", id: 3 },
-    { title: "the secret", id: 4 },
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brandon sanderson", id: 2 },
   ]);
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuid() }]);
+  };
+  const removeBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
+  };
+
   return (
-    <BookContext.Provider value={{ books }}>
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
       {props.children}
     </BookContext.Provider>
   );
