0177c54d6657c5c5376506b045d810df81db9bb6
SunilPrakash <sunil.prakash@talentica.com>
2020-04-07 13:56:20 +0530

    Creating context with functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 
 import { ThemeContext } from "../contexts/ThemeContext";
+import { BookContext } from "../contexts/BookContext";
 
 export const BookList = () => {
+  const { books } = useContext(BookContext);
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
   return (
@@ -11,9 +13,13 @@ export const BookList = () => {
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map((book) => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
---
+++ src/contexts/BookContext.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react";
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, setBooks] = useState([
+    { title: "almost home", id: 1 },
+    { title: "memory gospel", id: 2 },
+    { title: "this wild darkness", id: 3 },
+    { title: "the secret", id: 4 },
+  ]);
+  return (
+    <BookContext.Provider value={{ books }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
